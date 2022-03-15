71187218be5f5fbf2a18fc89bdeedc85234a71d1
SzymonRogozinski <pokeszymon.pl@gmail.com>
2022-03-15 17:10:37 +0100

    Coś dodałem

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+typedef struct graf {
+	int x; //liczba kolumn
+	int y; //liczba wierszy
+	//int n; Liczba grafów
+	int*** w; //wierzcho³ki
+}GF;
+
 int main() {
 	return 0;
 }
