76e5bbe3067a563073c8e8b03687bccc8d114362
TPGPL <mateuszkwiatkowski2002@gmail.com>
2022-04-01 00:19:44 +0200

    Implement priority queue

---
+++ src/prqueue.h
@@ -0,0 +1,18 @@
+#ifndef _PRQUEUE_H_
+#define _PRQUEUE_H_
+
+#include <float.h>
+
+typedef struct Kolejka_Priorytetowa
+{
+    int *queue; // elementy kolejki
+    int size;   // maks. rozmiar kolejki
+    int n;      // liczba elementów w kolejce
+} prqueue_t;
+
+prqueue_t *zainicjalizuj_kolejke_prio(int); // inicjalizuje kolejkę o określonym rozmiarze
+int dodaj_element(prqueue_t *, int);        // dodaje element do kolejki
+int usun_element(prqueue_t *, double *);    // wyjmuje element z kolejki
+int czy_pusta(prqueue_t *);                 // sprawdza, czy w kolejce są jakieś elementy; 0 - tak, 1 - nie
+
+#endif
