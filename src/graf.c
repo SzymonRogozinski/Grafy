a28b24af2b6ad7f88cf2307344f17e68d2a4e192
SzymonRogozinski <pokeszymon.pl@gmail.com>
2022-03-31 19:48:01 +0200

    Dzielenie grafu powinno działać, potzreba tylko więcej testów

--- src/graf.c
+++ src/graf.c
@@ -420,23 +420,38 @@ int dziel_graf(graph_t* G) {
             }
         }
     } while (ile_sasiadow(G, r) == 4);
-    if (n==2) { //Jeśli są tylko dwa wierzchołki, czyli pętla skończyła działanie po jednym wykonaniu
-        if (ile_sasiadow(G,trail[0]) + ile_sasiadow(G,trail[1])==2) //Wierzchołki są połączone tylko ze sobą
-            return zerwanie_polaczenia(G, trail[0],trail[1]);
-        else{
+    if (n == 2) { //Jeśli są tylko dwa wierzchołki, czyli pętla skończyła działanie po jednym wykonaniu
+        if (ile_sasiadow(G, trail[0]) + ile_sasiadow(G, trail[1]) == 2) //Wierzchołki są połączone tylko ze sobą
+            return zerwanie_polaczenia(G, trail[0], trail[1]);
+        else {
             //Sprawdzanie połączeń
             for (i = 0; i < 2; i++) {
-                for (int j=0;j<ile_sasiadow(G, trail[i]);j++) {
-                    if (ile_sasiadow(G, G->w[trail[i]][j * 2])==1) //Czy jeden z wierzchołków jest jedynym połączeniem z jakimś wierzchołkiem
+                for (int j = 0; j < ile_sasiadow(G, trail[i]); j++) {
+                    if (ile_sasiadow(G, G->w[trail[i]][j * 2]) == 1) //Czy jeden z wierzchołków jest jedynym połączeniem z jakimś wierzchołkiem
                         return zerwanie_polaczenia(G, trail[i], G->w[trail[i]][j * 2]);
                 }
             }
-            //Oderwanie tych dwóch wierzchołków nie spowoduje podzielenia grafu na więcej niż 2
+            //Oderwanie tych dwóch wierzchołków nie spowoduje podzielenia grafu na więcej niż 2, odrywam połączenia zewnętrzne
+            for (i = 0; i < 2; i++) {
+                for (int j = 0; j < ile_sasiadow(G, trail[i]); j++)
+                    zerwanie_polaczenia(G, trail[i], G->w[trail[i]][j * 2]);
+            }
         }
     }
-    for (i = 0; i < n; i++)
-        printf("%d: ",trail[i]);
-    printf("\n");
+    else{
+        //Sprawdzanie połączeń
+        for (i = 0; i < n; i++) {
+            for (int j = 0; j < ile_sasiadow(G, trail[i]); j++) {
+                if (ile_sasiadow(G, G->w[trail[i]][j * 2]) == 1) //Czy jeden z wierzchołków jest jedynym połączeniem z jakimś wierzchołkiem
+                    return zerwanie_polaczenia(G, trail[i], G->w[trail[i]][j * 2]);
+            }
+        }
+        //Jeśli wszystko dobrze, to tnij wzdłuż ścieżki
+        for (i = 0;i<n-1;i++) {
+            if (!zerwanie_polaczenia(G,trail[i],trail[i+1]))
+                return 0;
+        }
+    }
     free(trail);
     return 1;
 }
@@ -448,19 +463,21 @@ int zerwanie_polaczenia(graph_t* G,int q,int p) {
     y = szukaj_wierzcholek(p, q, G);
     if (x<0 || y<0) 
         return 0;
-    n = ile_sasiadow(G, q);
-    m = ile_sasiadow(G, p);
+    n = ile_sasiadow(G, q); //Ile sąsiadów q
+    m = ile_sasiadow(G, p); //Ile sąsiadów p
     //Przesuwanie tablicy wierzchołka q
-    for (int i=x+2;i<=n;i+=2) {
-        G->w[q][x] = G->w[q][i];
-        G->w[q][x+1] = G->w[q][i+1];
+    for (int i=x;i<n*2;i+=2) {
+        G->w[q][i] = G->w[q][i+2];
+        G->w[q][i+1] = G->w[q][i+3];
     }
+    G->w[q][n*2-1]=-1.0;
     //Przesuwanie tablicy wierzchołka p
-    for (int i = x + 2; i <= m; i += 2) {
-        G->w[p][x] = G->w[p][i];
-        G->w[p][x + 1] = G->w[p][i + 1];
+    for (int i = x; i <m*2; i += 2) {
+        G->w[p][i] = G->w[p][i+2];
+        G->w[p][i + 1] = G->w[p][i + 3];
     }
-    return (G->w[q]=realloc(G->w[q],(n-2)*sizeof*G->w[q]))==NULL || (G->w[p] = realloc(G->w[p], (m-2) * sizeof * G->w[p])) == NULL;
+    G->w[q][m * 2 - 1] = -1.0;
+    return (G->w[q]=realloc(G->w[q],(n*2+1)*sizeof*G->w[q]))==NULL || (G->w[p] = realloc(G->w[p], (m*2+1) * sizeof * G->w[p])) == NULL;
 }
 
 //Liczy ile sąsiadów ma wierzchołek
