50171b0f5bbeec36ff37cd7967f8a86601955eec
TPGPL <mateuszkwiatkowski2002@gmail.com>
2022-04-03 02:50:51 +0200

    Use formatter on the header file

--- src/graf.h
+++ src/graf.h
@@ -14,21 +14,21 @@ typedef struct Graf
 	double **w;		 // lista sąsiedstwa dla wierzchołków i wag
 } graph_t;
 
-int sprawdz_integralnosc(graph_t *);	   // sprawdza, czy połączenia i wagi w grafie zgadzają się ze sobą (1 - tak, 0 - nie)
-int czy_sasiaduja(int, int, int, int);	   // sprawdza, czy dwa wierzchołki sąsiadują ze sobą w grafie-siatce (1 - tak, 0 - nie)
-void zainicjalizuj_graf(graph_t *);		   // inicjalizuje zmienne w strukturze na wartości tymczasowe
-int wczytaj_graf(FILE *, graph_t *);	   // wczytuje graf z pliku
-void zapisz_graf(FILE *, graph_t *);	   // zapisuje graf do pliku
-void zwolnij_graf(graph_t *);			   // zwalnia pamięć po strukturach grafu
-int znajdz_droge_bfs(graph_t *, int, int); // sprawdza, czy istnieje droga między dwoma wierzchołkami; 0 - nie, 1 - tak
-void wyznacz_n_siatki(graph_t *);		   // wyznacza liczbę spójnych grafów w siatce
-void znajdz_droge(graph_t *, int, int);	   // wyznacza najkrótszą drogę między dwoma wierzchołkami
-void wyswietl_sciezke(int *, int);		   // wyświetla ścieżkę poprzedników
-int generuj_graf(graph_t *);	//Generuje graf o podanych parametrach
-int szukaj_wierzcholek(int, int, graph_t*);		//Szuka wierzchołka w tablicy sąsiedztwa innego wierzchołka
-double losuj(double,double);		//Losuje liczbę z podanego zakresu
-int dziel_graf(graph_t*);			//Funkcja dzieląca jeden graf na dwa
-int ile_sasiadow(graph_t*, int); //Liczy ile sąsiadów ma wierzchołek
-int zerwanie_polaczenia(graph_t*, int, int); //Zrywa połączenie między dwoma punktami. Zwraca 1 jeśli sukces
+int sprawdz_integralnosc(graph_t *);		  // sprawdza, czy połączenia i wagi w grafie zgadzają się ze sobą (1 - tak, 0 - nie)
+int czy_sasiaduja(int, int, int, int);		  // sprawdza, czy dwa wierzchołki sąsiadują ze sobą w grafie-siatce (1 - tak, 0 - nie)
+void zainicjalizuj_graf(graph_t *);			  // inicjalizuje zmienne w strukturze na wartości tymczasowe
+int wczytaj_graf(FILE *, graph_t *);		  // wczytuje graf z pliku
+void zapisz_graf(FILE *, graph_t *);		  // zapisuje graf do pliku
+void zwolnij_graf(graph_t *);				  // zwalnia pamięć po strukturach grafu
+int znajdz_droge_bfs(graph_t *, int, int);	  // sprawdza, czy istnieje droga między dwoma wierzchołkami; 0 - nie, 1 - tak
+void wyznacz_n_siatki(graph_t *);			  // wyznacza liczbę spójnych grafów w siatce
+void znajdz_droge(graph_t *, int, int);		  // wyznacza najkrótszą drogę między dwoma wierzchołkami
+void wyswietl_sciezke(int *, int);			  // wyświetla ścieżkę poprzedników
+int generuj_graf(graph_t *);				  // Generuje graf o podanych parametrach
+int szukaj_wierzcholek(int, int, graph_t *);  // Szuka wierzchołka w tablicy sąsiedztwa innego wierzchołka
+double losuj(double, double);				  // Losuje liczbę z podanego zakresu
+int dziel_graf(graph_t *);					  // Funkcja dzieląca jeden graf na dwa
+int ile_sasiadow(graph_t *, int);			  // Liczy ile sąsiadów ma wierzchołek
+int zerwanie_polaczenia(graph_t *, int, int); // Zrywa połączenie między dwoma punktami. Zwraca 1 jeśli sukces
 
 #endif
